cafc23cab14cec1589e67114bffbc3d4dfa4da3b
Talabi Toyyib Akorede <akoredetoyyib2001@gmail.com>
2020-09-04 10:50:59 +0100

    website

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let about = $('#about');
+let education = $('#education');
+let skills = $('#skills');
+let services = $('#services');
+let works = $('#works');
+let handles = $('#handles');
+let toyyib = $('#toyyib');
+let web = $('#web');
+
+about.click(function {
+    display
+});
